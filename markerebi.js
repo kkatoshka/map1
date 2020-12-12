656364834f35e81d53a26b511ba4e3210630fdf5
unknown <you@example.com>
2020-12-12 22:23:42 +0400

    Initial commit

---
+++ markerebi.js
@@ -0,0 +1,82 @@
+var Markers = [
+    {
+        "saxeli": "Tbilisi",
+        "lat": 41.71321947,
+        "lng": 44.80142892
+    },
+    {
+        "saxeli": "Sighnaghi",
+        "lat": 41.62143251,
+        "lng": 45.91336103
+    },
+    {
+        "saxeli": "Khashuri",
+        "lat": 41.99671793,
+        "lng": 43.58067422
+    },
+    {
+        "saxeli": "Gudauri",
+        "lat": 42.48572794,
+        "lng": 44.47679549
+    },
+    {
+        "saxeli": "Shovi",
+        "lat": 42.70675172,
+        "lng": 43.68691535
+    },
+    {
+        "saxeli": "Khulo",
+        "lat": 41.65940508,
+        "lng": 42.29654163
+    },
+    {
+        "saxeli": "Batumi",
+        "lat": 41.62833828,
+        "lng": 41.61752191
+    },
+    {
+        "saxeli": "Ambrolauri",
+        "lat": 42.52488922,
+        "lng": 43.1395423
+    },
+    {
+        "saxeli": "Zestafoni",
+        "lat": 42.12703435,
+        "lng": 43.0148244
+    },
+    {
+        "saxeli": "Java",
+        "lat": 42.40733179,
+        "lng": 43.93173199
+    },
+    {
+        "saxeli": "Rkoni",
+        "lat": 41.82828366,
+        "lng": 44.24352676
+    },
+    {
+        "saxeli": "Bakuriani",
+        "lat": 41.76457557,
+        "lng": 43.5229344
+    },
+    {
+        "saxeli": "Manglisi",
+        "lat": 41.69735525,
+        "lng": 44.37748303
+    },
+    {
+        "saxeli": "Chiatura",
+        "lat": 42.3134685,
+        "lng": 43.26656979
+    },
+    {
+        "saxeli": "Roshka",
+        "lat": 42.49594629,
+        "lng": 44.91561774
+    },
+    {
+        "saxeli": "Juta",
+        "lat": 42.58953671,
+        "lng": 44.74239842
+    }
+]
